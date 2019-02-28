21f2aa29abe88af810032e10d9091e365f82da55
MichaelWoutersDematic <michael.wouters@dematic.com>
2019-02-28 11:37:27 +0100

    Update GradeBookType.cs

--- GradeBook/Enums/GradeBookType.cs
+++ GradeBook/Enums/GradeBookType.cs
@@ -1,11 +1,6 @@
 namespace GradeBook.Enums
     public enum GradeBookType
     {
-    Standard,
-    Ranked,
-    ENSU,
-    OneToFour,
-    SixPoint
         Standard,
         Ranked,
         ESNU,
