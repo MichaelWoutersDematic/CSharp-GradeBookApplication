3aa357393eef98e2331b41038db7329f98ae6a30
MichaelWoutersDematic <michael.wouters@dematic.com>
2019-03-12 14:14:50 +0100

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -16,6 +16,8 @@ namespace GradeBook.GradeBooks
         {
             if (Students.Count < 5)
                 throw new InvalidOperationException("Ranked grading requires at least five students.");
+
+
             var threshold = (int)Math.Ceiling(Students.Count * 0.2);
             var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e=> e.AverageGrade).ToList();
             if (grades[threshold - 1] <= averageGrade)
